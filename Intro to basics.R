a43262c112387e160d4ef188da70cbf1d11fede3
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2022-01-28 18:49:57 -0800

    added to intro to basics R file

---
+++ Intro to basics.R
@@ -0,0 +1,72 @@
+# An addition
+5 + 5 
+
+# A subtraction
+5 - 5 
+
+# A multiplication
+3 * 5
+
+# A division
+(5 + 5) / 2 
+
+# Exponentiation
+2^5
+
+# Modulo
+28%%6
+
+# Assign the value 42 to x
+x <- 42
+
+# Print out the value of the variable x
+x
+
+# Assign the value 5 to the variable my_apples
+my_apples <- 5
+
+# Print out the value of the variable my_apples
+my_apples
+
+# Assign a value to the variables my_apples and my_oranges
+my_apples <- 5
+my_oranges <- 6
+
+# Add these two variables together
+my_apples + my_oranges  # prints 11
+
+# Create the variable my_fruit
+my_fruit <- my_apples + my_oranges
+
+# Assign a value to the variable my_apples
+my_apples <- 5 
+
+# Fix the assignment of my_oranges
+my_oranges <- 6
+
+# Create the variable my_fruit and print it out
+my_fruit <- my_apples + my_oranges 
+my_fruit  # prints 11
+
+# Change my_numeric to be 42
+my_numeric <- 42
+
+# Change my_character to be "universe"
+my_character <- "universe"
+
+# Change my_logical to be FALSE
+my_logical <- FALSE
+
+# Declare variables of different types
+my_numeric <- 42
+my_character <- "universe"
+my_logical <- FALSE 
+
+# Check class of my_numeric
+class(my_numeric)  # prints "numeric"
+
+# Check class of my_character
+class(my_character)  # prints "character"
+
+# Check class of my_logical
+class(my_logical)  # prints "logical"
