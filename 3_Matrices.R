f409039e00485db424f43a527f82e4d755a755e2
Taylor Johnson <taylor.johnson.veteran@outlook.com>
2022-01-29 22:17:14 -0800

    updated the first 3 lines of comments

--- 3_Matrices.R
+++ 3_Matrices.R
@@ -1,6 +1,6 @@
-# A matrix is a collection of elements of the same data type (numeric, character, or logical) arranged into a fixed number of rows and columns. 
-# Since you are only working with rows and columns, a matrix is called two-dimensional.
-# Construct a matrix with the matrix() function.
+# A matrix is a 2-D array of rows and columns.
+# A matrix is a collection of elements of the same data type (numeric, character, or logical-true/false) arranged into a fixed number of rows and columns. 
+# Use the matrix() function to construct a matrix.
 
 # Construct a matrix with 3 rows that contain the numbers 1 up to 9
 matrix(1:9, byrow = TRUE, nrow = 3)
